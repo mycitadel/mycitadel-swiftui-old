26f628fb696e3b52e8c25a32a5b0e32d820c286b
Dr Maxim Orlovsky <orlovsky@mycitadel.io>
2020-11-14 19:46:01 +0100

    Prototype working on iOS14

--- mycitadel/ContentView.swift
+++ mycitadel/ContentView.swift
@@ -9,34 +9,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    static let Tab1Title = "Assets & Balances"
+    static let Tab2Title = "Send & Receive"
+
     @State private var selection = 0
- 
+    
     var body: some View {
         TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
+            NavigationView {
+                AssetsView(assets.map {
+                    AssetCard(asset: $0, brief: false)
+                        .frame(minWidth: 113, idealWidth: 266, maxWidth: 331, minHeight: 131, idealHeight: 166, maxHeight: 213)
+                        .padding()
+                })
+                .navigationBarTitle(Self.Tab1Title)
+            }
+            .tabItem {
+                VStack {
+                    Image(systemName: "bitcoinsign.circle" + (selection == 0 ? ".fill" : "")).font(.title)
+                    Text(Self.Tab1Title)
                 }
-                .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
+            }
+            .tag(0)
+            TransferView(assets.map {
+                AssetCard(asset: $0, brief: true)
+                    .frame(minWidth: 113, idealWidth: 266, maxWidth: 331, minHeight: 66, idealHeight: 113, maxHeight: 213)
+                    .padding()
+            })
+            .tabItem {
+                VStack {
+                    Image(systemName: "arrow.up.arrow.down.circle" + (selection == 1 ? ".fill" : "")).font(.title)
+                    Text(Self.Tab2Title)
                 }
-                .tag(1)
-        }
+            }
+            .tag(1)
+        }.accentColor(.purple)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+           ContentView()
+              .environment(\.colorScheme, .light)
+
+           ContentView()
+              .environment(\.colorScheme, .dark)
+        }
     }
 }
